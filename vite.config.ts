868a7c94a9604c37e11e1cd1e36faa42eb7baa68
fdemb <69842744+fdemb@users.noreply.github.com>
2026-02-08 17:47:36 +0100

    chore: use bun, add a deploy ci

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  base: process.env.GITHUB_ACTIONS ? "/rigid-ui/" : "/",
   server: {
     port: 3333,
   },
